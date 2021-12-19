1b23fe95c896208ba762d5d6e127c62b4d71c638
nuberchardzer <nuberchardzer@gmail.com>
2021-12-19 17:41:21 +0300

    bot

---
+++ mybot/bot.py
@@ -0,0 +1,20 @@
+import logging
+from telegram.ext import Updater, CommandHandler
+logging.basicConfig(filename='bot.pristav', level=logging.INFO)
+PROXY = {'proxy_url': 'socks5://t2.learn.python.ru:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+def main():
+    mybot = Updater("5046863773:AAEiiHs9-1YPrAYAVTqcyKgtG29JPrgro2Y", use_context=True, request_kwargs=PROXY)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    logging.info("Бот стартовал")
+    mybot.start_polling()
+    mybot.idle()
+
+def greet_user(update, context):
+    print('Вызван /start')
+    update.message.reply_text('Привет! Я Бот-пристав. Проверим ваши задолженности?')
+
+main()
